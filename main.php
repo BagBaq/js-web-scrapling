a80462f2925048a7625824778108c6ae12d1da31
Mücahit <46865744+BagBaq@users.noreply.github.com>
2022-07-23 20:45:54 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,36 +1,36 @@
 <?php
 
-$url = "https://www.mobilhanem.com/test/php-dersleri/"; // web Site URL
-$veri =  file_get_contents($url); // verimiz geldi
+$url = "https://40saniye.com/gta-san-andreas-tum-hileler-pc"; // web Site URL
+$data =  file_get_contents($url); // verimiz geldi
 
-$pattern = '@<article (.*?)>(.*?)</article>@si';
+$contentPattern = '@<div class="post-text text-style" (.*?)>(.*?)</div>@si';
 
-preg_match_all($pattern,$veri,$yazilar);
+preg_match_all($pattern,$veri,$contents);
 
 $posts = array();
-for($i=0;$i<count($yazilar[0]);$i++) {
+for($i=0;$i<count($contents[0]);$i++) {
 	// başlığı ve url alıyoruz.
-	$baslikPattern = '@<a itemprop="url" href="(.*?)" (.*?)>(.*?)</a>@si';
-	preg_match($baslikPattern,$yazilar[0][$i],$baslik);
+	$titlePattern = '@<a itemprop="url" href="(.*?)" (.*?)>(.*?)</a>@si';
+	preg_match($titlePattern,$contents[0][$i],$title);
 	
 	// resimi alıyoruz. 
-	$resimPattern = '@<img alt="(.*?)" width="250" height="160" src="(.*?)">@si';
-	preg_match($resimPattern,$yazilar[0][$i],$resim);
+	$thumbnailPattern = '@<img alt="(.*?)" width="250" height="160" src="(.*?)">@si';
+	preg_match($thumbnailPattern,$contents[0][$i],$thumbnail);
 	
 	
 	
 	// posts diye bir diziye verileri atalım daha sonra kendimiz ayarlayalım. 
 	
-	$posts[$i]["baslik"] = $baslik[3];
-	$posts[$i]["post_url"] = $baslik[1];
-	$posts[$i]["resim"] = $resim[2];
+	$posts[$i]["title"] = $baslik[3];
+	$posts[$i]["content"] = $content[1];
+	$posts[$i]["thumbnail"] = $resim[2];
 }
 
 
 /// alınan verileri ekranda gösteriyoruz. 
 echo '<ul style="list-style:none;">';
 foreach( $posts as $post) {
-	echo ' <li> <img src="'.str_replace('./',$url,$post["resim"]).'" alt="" /> <a href="post_detay.php?url='.$post["post_url"].'">'.$post["baslik"]."</a></li>";
+	echo ' <li> <img src="'.str_replace('./',$url,$post["thumbnail"]).'" alt="" /> <a href="post_detay.php?url='.$post["post_url"].'">'.$post["title"]."</a></li>";
 }
 echo '</ul>';
 ?>
